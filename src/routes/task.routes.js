ea25492cbd62e80407781de17ad2d88b4c5d9f61
S4RMI618 <huessabe@gmail.com>
2023-09-15 00:18:58 -0500

    data validators added

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,12 +7,19 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/task.controllers.js";
+import { validateSchema } from "../middlewares/validate.middleware.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
 
 const router = Router();
 
 router.get("/tasks", authRequired, getTasks);
 router.get("/tasks/:id", authRequired, getTask);
-router.post("/task", authRequired, createTask);
+router.post(
+  "/task",
+  authRequired,
+  validateSchema(createTaskSchema),
+  createTask
+);
 router.put("/tasks/:id", authRequired, updateTask);
 router.delete("/tasks/:id", authRequired, deleteTask);
 
