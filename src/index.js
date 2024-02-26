1e6c4e43f4a0e9a3cd9e23c76b434efefa0463a0
Hugo Sarmiento <111538758+S4RMI618@users.noreply.github.com>
2024-02-26 14:50:29 -0500

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-import {connectDB} from './db.js';
-import { BackendPORT } from './config.js';
-
-connectDB();
-app.listen(BackendPORT)
-console.log('server on port', BackendPORT)
